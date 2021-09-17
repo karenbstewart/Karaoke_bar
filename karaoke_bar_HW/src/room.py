409a669cca524c903ea2d6319761eb8c7e008077
Karen Stewart <kastewart2002@googlemail.com>
2021-09-17 19:03:38 +0100

    tests for MVP passed

--- karaoke_bar_HW/src/room.py
+++ karaoke_bar_HW/src/room.py
@@ -1,11 +1,34 @@
+
+
 class Room:
     def __init__(self, input_name, input_capacity, input_entry_fee):
         self.name = input_name     
         self.capacity = input_capacity
         self.entry_fee = input_entry_fee
         self.song_list = []
-        self.guest = []
+        self.guest_list = []
 
     def add_song_to_room(self, song):
         self.song_list.append(song)
+
+    def check_in(self, customer):
+        self.guest_list.append(customer)
+
+    def check_find_guest_by_name(self, name):
+        for guest in self.guest_list:
+            if guest.name == name:
+                return True
+            return False
+
+
+    def check_out(self, singer):
+        is_in_list = self.check_find_guest_by_name(singer)
+        if is_in_list == True:
+            self.guest_list.remove(singer)
+        if is_in_list == False:
+            self.guest_list
+    
+
+    
+
 
--- karaoke_bar_HW/tests/room_tests.py
+++ karaoke_bar_HW/tests/room_tests.py
@@ -1,12 +1,16 @@
 import unittest
 from src.room import Room 
 from src.song import Song
+from src.guest import Guest
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
         self.room1 = Room("Disco room", 30, 15)
+        self.room2 = Room("RnB room", 10, 17)
         self.song1 = Song("closing time", "Semisonic")
         self.song2 = Song("All the small things", "Blink 182")
+        self.guest1 = Guest("Jim", 100, "closing time")
+        self.guest2 = Guest("Arthur", 150, "Hooked on a feeling")
 
     def test_room_has_fee(self):
         self.assertEqual(15, self.room1.entry_fee)
@@ -14,4 +18,22 @@ class TestRoom(unittest.TestCase):
     def test_can_add_song_to_room(self):
         self.room1.add_song_to_room(self.song1)
         self.room1.add_song_to_room(self.song2)
-        self.assertEqual(2, len(self.room1.song_list) )
+        self.assertEqual(2, len(self.room1.song_list))
+
+    # def test_check_in(self):
+    #     self.room1.check_in(self.guest1)
+    #     self.assertEqual(1 , len(self.room1.guest_list))
+
+    def test_guest_has_checked_in(self):
+        self.room1.check_in(self.guest1)
+        is_guest_checked_in = self.room1.check_find_guest_by_name(self.guest1.name) 
+        self.assertEqual(True , is_guest_checked_in)
+
+    def test_guest_has_checked_out(self):
+        self.room2.check_in(self.guest1)
+        self.room2.check_in(self.guest2)
+        self.room2.check_out(self.guest2)
+        is_guest_checked_out = self.room2.check_find_guest_by_name(self.guest2.name)
+        self.assertEqual(False, is_guest_checked_out)
+
+
