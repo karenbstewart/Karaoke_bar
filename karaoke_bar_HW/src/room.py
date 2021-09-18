788479b59e803a64228f8e6936a981327faeeb45
Karen Stewart <kastewart2002@googlemail.com>
2021-09-18 17:39:03 +0100

    passing MvP

--- karaoke_bar_HW/src/room.py
+++ karaoke_bar_HW/src/room.py
@@ -7,12 +7,16 @@ class Room:
         self.entry_fee = input_entry_fee
         self.song_list = []
         self.guest_list = []
+        self.wait_queue_list = []
 
     def add_song_to_room(self, song):
         self.song_list.append(song)
 
     def check_in(self, customer):
-        self.guest_list.append(customer)
+        if len(self.guest_list) <= self.capacity:
+            self.guest_list.append(customer)
+        if len(self.guest_list) > self.capacity:
+            self.wait_queue_list.append(customer)
 
     def check_find_guest_by_name(self, name):
         for guest in self.guest_list:
@@ -20,14 +24,15 @@ class Room:
                 return True
             return False
 
-
     def check_out(self, singer):
         is_in_list = self.check_find_guest_by_name(singer)
         if is_in_list == True:
             self.guest_list.remove(singer)
         if is_in_list == False:
             self.guest_list
-    
+
+    # def can_guest_enter_this_room(self, customer, wait_list):
+
 
     
 
--- karaoke_bar_HW/tests/room_tests.py
+++ karaoke_bar_HW/tests/room_tests.py
@@ -5,12 +5,18 @@ from src.guest import Guest
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
-        self.room1 = Room("Disco room", 30, 15)
+        self.room1 = Room("Disco room", 13, 15)
         self.room2 = Room("RnB room", 10, 17)
+        self.room3 = Room("ickle room", 4, 25)
         self.song1 = Song("closing time", "Semisonic")
         self.song2 = Song("All the small things", "Blink 182")
-        self.guest1 = Guest("Jim", 100, "closing time")
-        self.guest2 = Guest("Arthur", 150, "Hooked on a feeling")
+        self.song3 = Song("Call it dreaming", "Iron & Wine")
+        self.guest1 = Guest("Dan", 100, "closing time")
+        self.guest2 = Guest("Björn", 150, "Hooked on a feeling")
+        self.guest3 = Guest("Jimi", 12, "Purple Haze")
+        self.guest4 = Guest("Thomas", 300, "Die a happy man")
+        self.guest5 = Guest("Mark", 3, "If")
+        self.guest6 = Guest("Samuel", 1000, "Call it dreaming")
 
     def test_room_has_fee(self):
         self.assertEqual(15, self.room1.entry_fee)
@@ -20,9 +26,9 @@ class TestRoom(unittest.TestCase):
         self.room1.add_song_to_room(self.song2)
         self.assertEqual(2, len(self.room1.song_list))
 
-    # def test_check_in(self):
-    #     self.room1.check_in(self.guest1)
-    #     self.assertEqual(1 , len(self.room1.guest_list))
+    def test_check_in(self):
+        self.room1.check_in(self.guest1)
+        self.assertEqual(1 , len(self.room1.guest_list))
 
     def test_guest_has_checked_in(self):
         self.room1.check_in(self.guest1)
@@ -35,5 +41,16 @@ class TestRoom(unittest.TestCase):
         self.room2.check_out(self.guest2)
         is_guest_checked_out = self.room2.check_find_guest_by_name(self.guest2.name)
         self.assertEqual(False, is_guest_checked_out)
+
+    # def test_too_many_guest_in_room(self):
+    #     self.room3.check_in(self.guest1)
+    #     self.room3.check_in(self.guest2)
+    #     self.room3.check_in(self.guest3)
+    #     self.room3.check_in(self.guest4)
+    #     self.room3.check_in(self.guest5)
+    #     self.room3.can_guest_enter_this_room(self.guest6, self.room3.wait_queue)
+    #     self.assertEqual(2, len(self.room3.wait_queue))
+
+
 
 
